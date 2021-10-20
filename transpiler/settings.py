44631b0b4d1bbd8d5349eba24b3b763db6d2a971
Houida <58174147+houida9@users.noreply.github.com>
2021-10-20 14:56:10 -0400

    Update settings.py

--- transpiler/settings.py
+++ transpiler/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f2i5(m_+5*484jjr$&25-s@om#aeydc*blz0tea^6a32yv+4!m'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
